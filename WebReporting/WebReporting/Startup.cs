9aaca678e9a2842fee14c51ffdff3170a4ad43a0
Saftec Notebook <sistemas@saftec.com.ar>
2016-12-21 13:12:42 -0300

    Creacion de proyecto.

---
+++ WebReporting/WebReporting/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebReporting.Startup))]
+namespace WebReporting
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
